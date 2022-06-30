08ccc3e1c88815e843f7040c32dddd382cac0328
Lamvo27072001 <lampham27072001@gmail.com>
2022-07-01 00:01:10 +0700

    Finish

--- script.js
+++ script.js
@@ -1,31 +1,75 @@
 let display = document.getElementById('operand')
-let result = document.getElementById("result")
+let previous = document.getElementById("previous")
 let numbers = document.getElementsByClassName("number")
 let operators = document.getElementsByClassName("operator")
 let clear = document.getElementById("clear")
 let dots = document.getElementsByClassName("dot")
 let del = document.getElementById("delete")
 let equal = document.getElementById("equal")
+let currentOperator
+let first = ""
+let second = ""
+
 
 for (let number of numbers) {
     number.addEventListener("click", () => {
-        display.innerHTML += number.innerHTML
+
+        if (currentOperator) {
+            if (second == "") {
+                display.innerHTML = number.innerHTML
+            } else {
+                display.innerHTML += number.innerHTML
+            }
+            second += number.innerHTML
+        } else {
+            first += number.innerHTML
+            console.log(first)
+            display.innerHTML += number.innerHTML
+        }
+
+        setColor(null)
     })
 
 }
+
+
 for (let operate of operators) {
-    operate.addEventListener("click", () => {
-        display.innerHTML += operate.innerHTML
+    operate.addEventListener("click", (e) => {
+
+        currentOperator = e.target
+        setColor(currentOperator)
 
     })
 }
+
+function setColor(currentOperators) {
+    for (let operate1 of operators) {
+        if (currentOperators === operate1) {
+            operate1.style.backgroundColor = "white"
+        } else {
+            operate1.style.backgroundColor = "rgba(255, 255, 255, .75)"
+        }
+    }
+}
+
 clear.onclick = () => {
     display.innerHTML = ""
+    second = ""
+    first = ""
+    currentOperator = null
 }
 
 dots[0].onclick = () => {
 
     display.innerHTML += "."
+    if (currentOperator) {
+
+        second += "."
+    } else {
+        first += "."
+
+    }
+
 }
 
 // for (let dot of dots) {
@@ -34,14 +78,35 @@ dots[0].onclick = () => {
 //     })
 // }
 
-
 del.onclick = () => {
     display.innerHTML = display.innerHTML.slice(0, -1)
 
+    if (currentOperator) {
+
+        second = second.slice(0, -1)
+    } else {
+        first = first.slice(0, -1)
+
+    }
 }
 
 equal.onclick = () => {
-    if (operate.innerHTML == "+") {
-        result.innerHTML += numbers.innerHTML
+    switch (currentOperator.innerHTML) {
+        case '+':
+            display.innerHTML = first * 1 + second * 1
+            console.log(first, second)
+            break;
+        case '-':
+            display.innerHTML = first * 1 - second * 1
+            break;
+        case '*':
+            display.innerHTML = (first * 1) * (second * 1)
+            break
+        case '÷':
+            display.innerHTML = (first * 1) / (second * 1)
+            break
     }
+    second = ""
+    first = ""
+    currentOperator = null
 }
